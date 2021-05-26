69f0102032a2797ec60e0db6df6c4c7b8f7a74bc
Natalie Finnegan <nfinneg2@gmail.com>
2021-05-26 15:36:15 -0600

    est. localhost connection

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+//const logger = require("morgan");
+const mongoose = require("mongoose");
+const path = require("path");
+
+const PORT = process.env.PORT || 3000;
+
+//const db = require("./models");
+
+// Sets up the Express App
+const app = express();
+
+//app.use(logger("dev"));
+
+//Body Parser Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
