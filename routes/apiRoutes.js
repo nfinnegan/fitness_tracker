d8a60b2e86aa28be51817890f88f98ab16584685
Natalie Finnegan <nfinneg2@gmail.com>
2021-05-27 09:51:06 -0600

    created index and 1st api route

---
+++ routes/apiRoutes.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+const Workout = require("../models/workoutModel");
+
+//create new workout
+router.get("/exercise", ({ body }, res) => {
+  Workout.create(body)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
