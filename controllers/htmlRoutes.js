15804c57bde58e0b949ceeaa0715ef6fce3fe2d9
Natalie Finnegan <nfinneg2@gmail.com>
2021-05-27 16:25:30 -0600

    added stats & default route

--- controllers/htmlRoutes.js
+++ controllers/htmlRoutes.js
@@ -9,4 +9,13 @@ router.get("/exercise", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
+router.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+// If no matching route is found default to home
+router.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
 module.exports = router;
