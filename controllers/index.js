ae1227d06addb84f859b94715d9331af89b1b7b3
Natalie Finnegan <nfinneg2@gmail.com>
2021-05-27 18:13:20 -0600

    working on first api route

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
-//const apiRoutes = require("./apiRoutes");
+const apiRoutes = require("./api/apiRoutes");
 const htmlRoutes = require("./htmlRoutes");
 
-//router.use("/api", apiRoutes);
+router.use("/api", apiRoutes);
 router.use("/", htmlRoutes);
 
 module.exports = router;
